ae37bcf7012bdf6dbb5365fa47eb3e5ae1bbcd48
SerhiyMykuliak <serhijko90@gmail.com>
2025-05-14 11:36:09 +0300

    Add flash message with stimulus auto-dismiss

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,9 +3,3 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import MenuToggleController from "./menu_toggle_controller"
-application.register("menu-toggle", MenuToggleController)
-
-import CausesProgressBarController from "./causes_progress_bar"
-application.register("progress-bar", CausesProgressBarController)
-
