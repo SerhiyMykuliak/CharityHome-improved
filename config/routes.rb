942c921a56ad79bb0be56a2d4d5a393d872b0edf
SerhiyMykuliak <serhijko90@gmail.com>
2025-05-24 16:52:40 +0300

    Add contact form

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   post '/webhooks_monopay', to: 'webhooks#monopay', as: :webhooks_monopay
   
+  post '/contact-us', to: 'contacts#create'
 end
